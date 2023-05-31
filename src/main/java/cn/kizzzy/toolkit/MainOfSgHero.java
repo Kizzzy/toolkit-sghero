35744ab0b49f38358b89091412ef17de0566c847
Kizzzy <xuhua0174@163.com>
2023-05-31 11:18:29 +0800

    [sghero] * refactor javafx

--- src/main/java/cn/kizzzy/toolkit/MainOfSgHero.java
+++ src/main/java/cn/kizzzy/toolkit/MainOfSgHero.java
@@ -1,6 +1,6 @@
 package cn.kizzzy.toolkit;
 
-import cn.kizzzy.toolkit.controller.Controllers;
+import cn.kizzzy.javafx.plugin.PluginView;
 import cn.kizzzy.toolkit.controller.SgHeroLocalController;
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -9,6 +9,6 @@ public class MainOfSgHero extends Application {
     
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Controllers.start(null, primaryStage, SgHeroLocalController.class);
+        PluginView.show(null, primaryStage, SgHeroLocalController.class);
     }
 }
--- src/main/java/cn/kizzzy/toolkit/controller/SgHeroLocalController.java
+++ src/main/java/cn/kizzzy/toolkit/controller/SgHeroLocalController.java
@@ -1,16 +1,13 @@
 package cn.kizzzy.toolkit.controller;
 
+import cn.kizzzy.javafx.menu.MenuParameter;
+import cn.kizzzy.javafx.plugin.PluginParameter;
 import cn.kizzzy.javafx.viewer.ViewerExecutor;
 import cn.kizzzy.javafx.viewer.executor.RdfViewerExecutor;
 
 @MenuParameter(path = "文件浏览/三国豪侠传/解包器(本地)")
-@PluginParameter(url = "/fxml/viewer/explorer_view.fxml", title = "文件浏览(SGH)")
+@PluginParameter(title = "文件浏览(SGH)")
 public class SgHeroLocalController extends ExplorerView {
-    
-    @Override
-    public String getName() {
-        return "SgHero Display";
-    }
     
     @Override
     protected ViewerExecutor initialViewExecutor() {
